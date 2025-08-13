e9eab67b95aeaae5f615199985bdff6cfbe1a88a
eduardoberettarod <eduardo.berettarod@gmail.com>
2025-08-13 20:21:19 -0300

    feat
    arrumei os problemas

--- src/components/HandImage.jsx
+++ src/components/HandImage.jsx
@@ -2,12 +2,15 @@ import './HandImage.css';
 
 function HandImage() {
   return (
-    <>
-      <div className="container-hand">
+    <div className="container-hand">
+      <picture>
+        {/* Quando for até 767px, usa o hand2.svg */}
+        <source media="(max-width: 767px)" srcSet="src/assets/svg/hand2.svg" />
+        {/* Caso contrário, usa o hand.svg */}
         <img src="src/assets/svg/hand.svg" className="hand" alt="Mão" />
-      </div>
-    </>
-  )
+      </picture>
+    </div>
+  );
 }
 
 export default HandImage;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ function Navbar() {
 
         {/* Menu */}
         <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
-          <li><a onClick={closeMenu} href="index.html#home-page" className="nav-link">Início</a></li>
+          <li><a onClick={closeMenu} href="index.html#home-page" className="nav-link" id='li-inicio'>Início</a></li>
           <li><a onClick={closeMenu} href="index.html#sobre" className="nav-link">Sobre</a></li>
           <li><a onClick={closeMenu} href="index.html#projetos" className="nav-link">Projetos</a></li>
           <li><a onClick={closeMenu} href="index.html#serviços" className="nav-link">Serviços</a></li>
