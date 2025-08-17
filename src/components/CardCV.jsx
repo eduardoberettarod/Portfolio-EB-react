fa941177e189d4fbd4711222883f45e55e957ffc
eduardoberettarod <eduardo.berettarod@gmail.com>
2025-08-17 19:30:01 -0300

    feat
    adicionando caminho para o download do meu curriculo

--- src/components/CardCV.jsx
+++ src/components/CardCV.jsx
@@ -4,7 +4,11 @@ function CardCV() {
   return (
     <div className="card-info" id="cv">
       <h1>Baixar CV</h1>
-      <a href="curriculo.pdf" download="curriculo-eduardo-beretta.pdf" className="btn-download">
+      <a 
+        href="/documents/curriculo.pdf" 
+        download="curriculo-eduardo-beretta.pdf" 
+        className="btn-download"
+      >
         Download PDF
       </a>
     </div>
--- src/components/Redes.jsx
+++ src/components/Redes.jsx
@@ -4,17 +4,19 @@ function Redes() {
 
   return (
     <>
-    <div className="social-container">
-      <a href="https://www.linkedin.com/in/eduardo-beretta-a597ba356/" target="_blank" className="social-link">
-        <img src="./svg/linkedin.svg" alt="LinkedIn" className="social-icon" />
-      </a>
-      <a href="https://github.com/eduardoberettarod" target="_blank" className="social-link">
-        <img src="./svg/github.svg" alt="GitHub" className="social-icon" />
-      </a>
-      <a href="./assets/curiculo.pdf" download="curriculo-eduardo-beretta.pdf" className="social-link">
-        <img src="./svg/file-earmark-person-fill.svg" alt="Baixar Currículo" className="social-icon" />
-      </a>
-    </div>
+      <div className="social-container">
+        <a href="https://www.linkedin.com/in/eduardo-beretta-a597ba356/" target="_blank" className="social-link">
+          <img src="./svg/linkedin.svg" alt="LinkedIn" className="social-icon" />
+        </a>
+        <a href="https://github.com/eduardoberettarod" target="_blank" className="social-link">
+          <img src="./svg/github.svg" alt="GitHub" className="social-icon" />
+        </a>
+        <a href="/documents/curriculo.pdf"
+          download="curriculo-eduardo-beretta.pdf"
+          className="social-link">
+          <img src="./svg/file-earmark-person-fill.svg" alt="Baixar Currículo" className="social-icon" />
+        </a>
+      </div>
     </>
   )
 }
