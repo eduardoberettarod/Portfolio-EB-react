9d69b3edfeae473c483eaed8b36765168cbf9cb8
eduardoberettarod <eduardo.berettarod@gmail.com>
2025-08-17 18:58:29 -0300

    Feat
    adicionando um formulario funcional e documentos imoportantes

--- src/components/FormularioContato.jsx
+++ src/components/FormularioContato.jsx
@@ -1,21 +1,43 @@
 import './FormularioContato.css';
+import emailjs from 'emailjs-com';
+import { useRef } from 'react';
 
 function FormularioContato() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      'service_eduardo',     // Service ID
+      'template_o2k4ixp',    // Template ID
+      form.current,
+      'ipdcWkPMh0o-WwtED'    // Public Key
+    )
+    .then(() => {
+      alert("Mensagem enviada com sucesso!");
+      form.current.reset();
+    })
+    .catch((err) => {
+      alert("Erro ao enviar: " + err.text);
+    });
+  };
+
   return (
     <div className="card-form">
       <h1>Entre em contato</h1>
-      <form>
+      <form ref={form} onSubmit={sendEmail}>
         <label>Primeiro Nome</label>
-        <input type="text" placeholder="Seu primeiro nome*" required />
+        <input type="text" name="primeiro_nome" placeholder="Seu primeiro nome*" required />
 
         <label>Sobrenome</label>
-        <input type="text" placeholder="Seu sobrenome*" required />
+        <input type="text" name="sobrenome" placeholder="Seu sobrenome*" required />
 
         <label>E-mail</label>
-        <input type="email" placeholder="Seu e-mail*" required />
+        <input type="email" name="email" placeholder="Seu e-mail*" required />
 
         <label>Mensagem</label>
-        <textarea placeholder="Escreva sua mensagem*" required></textarea>
+        <textarea name="mensagem" placeholder="Escreva sua mensagem*" required></textarea>
 
         <button type="submit">Enviar</button>
       </form>
